a9cdfd06dd69bb0ad83f4bc48ec9642a5cde46c3
William Viker <william.viker@gmail.com>
2011-09-04 15:37:46 +0200

    session,login++

--- frontend/webui2/lib/webui2.pm
+++ frontend/webui2/lib/webui2.pm
@@ -4,8 +4,37 @@ use DB;
 
 our $VERSION = '0.1';
 
+before sub {
+    if (! session('user') && request->path_info !~ m{^/(login|logout)}) {
+        var requested_path => request->path_info;
+        request->path_info('/login');
+    }
+};
+ 
+get '/login' => sub {
+    template 'login', { path => vars->{requested_path} };
+};
+ 
+post '/login' => sub {
+    if (params->{user} eq 'admin' && params->{pass} eq 'admin') {
+        session user => "1";
+        redirect '/';
+    } else {
+        redirect '/login?failed=1';
+    }
+};
+
+get '/logout' => sub {
+	session user => 0;
+	redirect "/";
+};
+
 get '/' => sub {
 	template 'index';
+};
+
+get '/patch' => sub {
+	template 'patch';
 };
 
 true;
