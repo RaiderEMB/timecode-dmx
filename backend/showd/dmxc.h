5fd8ec0e2f4919bb24236f243745900958e3035d
Håkon Nessjøen <haakon.nessjoen@gmail.com>
2011-09-11 02:31:28 +0200

    First tests firing events scheduled from jackd via dmxd. dmxc api updated.

--- backend/showd/dmxc.h
+++ backend/showd/dmxc.h
@@ -18,8 +18,24 @@ enum dmxd_commands {
 	DMXD_FUNC_TRANSACTION_END
 };
 
+/* Easy api */
 int dmxc_init(char *hostname, unsigned short port);
 int dmxc_send(enum dmxd_commands command, ...);
 int dmxc_sendf(enum dmxd_commands command, char flag, ...);
+
+
+/* Advanced api */
+struct dmxc_packet {
+	unsigned short uniqueid;
+	int length;
+	char data[1024];
+};
+
+int dmxc_packet_init(struct dmxc_packet *packet, enum dmxd_commands command, char flag);
+int dmxc_packet_add_char(struct dmxc_packet *packet, unsigned char data);
+int dmxc_packet_add_short(struct dmxc_packet *packet, unsigned short data);
+int dmxc_packet_add_int(struct dmxc_packet *packet, unsigned int data);
+int dmxc_packet_add_float(struct dmxc_packet *packet, float data);
+int dmxc_udp_send(struct dmxc_packet *packet);
 
 #endif
--- backend/showd/test_dmxcontrol.pl
+++ backend/showd/test_dmxcontrol.pl
@@ -8,14 +8,25 @@ $DMXControl::host = '127.0.0.1';
 $DMXControl::port = 9118;
 
 DMXControl::init();
-while (1) {
 print "mainloop\n";
-for (1..44) {
 	DMXControl::start_transaction();
-	for my $ch (1..511) {
-		DMXControl::blink($ch,0,255,0.1,0.1,4,DMXControl::OVERRIDE);
-	}
+	#DMXControl::fade(1,1,15,10,DMXControl::OVERRIDE);
+	#DMXControl::lock(1,15,8,DMXControl::OVERRIDE);
+	#DMXControl::lock(2,30,10,DMXControl::OVERRIDE);
+	DMXControl::blink(2,5,90,0.5,0.5,10,DMXControl::OVERRIDE);
+	DMXControl::blink(1,0,15,0.5,0.5,10,DMXControl::OVERRIDE);
+	DMXControl::lock(3,90,10,DMXControl::OVERRIDE);
+	DMXControl::fade(4,0,255,10,DMXControl::OVERRIDE);
+	DMXControl::fade(5,0,155,10,DMXControl::OVERRIDE);
 	DMXControl::end_transaction();
-	usleep(23000);
+sleep(8);
+	DMXControl::start_transaction();
+	DMXControl::lock(2,0,2,DMXControl::OVERRIDE);
+	DMXControl::end_transaction();
+exit;
+while (1) {
+	print "Control\n";
+	DMXControl::control(DMXControl::OVERRIDE);
+	usleep(500000);
 }
-}
+	exit;
