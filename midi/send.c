3381e0668fec6abf25871b803f4aba76609b8f55
Håkon Nessjøen <haakon.nessjoen@gmail.com>
2012-04-23 18:38:56 +0200

    Should work now. But preliminary tests was unsuccessful. Have to run.

---
+++ midi/send.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <linux/input.h>
+
+unsigned char packet_signature[] = { 0x13, 0x37 };
+
+#define UDPMIDI_PORT 1205
+
+int main(int argc, char **argv) {
+	int sock;
+	struct sockaddr_in si_me, si_other;
+	int len, slen = sizeof(si_other);
+	int optval = 1;
+	unsigned char buf[10],outbuf[12];
+
+        if(argc < 2) {
+                fprintf(stderr, "%s <device>\n", argv[0]);
+                return -1;
+        }
+
+	if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+		perror("udp socket");
+		return 1;
+	}
+
+	memset((void *) &si_me, 0, sizeof(si_me));
+	si_me.sin_family = AF_INET;
+	si_me.sin_port = htons(0);
+	si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	memset((void *) &si_other, 0, sizeof(si_me));
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(UDPMIDI_PORT);
+	//si_other.sin_addr.s_addr = inet_aton("127.0.0.1");//htonl(INADDR_BROADCAST);
+	inet_aton("127.0.0.1", &si_other.sin_addr);
+
+	if (bind(sock, (const struct sockaddr*)&si_me, sizeof(si_me)) == -1) {
+		perror("udp port");
+		close(sock);
+		return 1;
+	}
+
+	if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &optval, sizeof (optval)) ==- 1) {
+		perror("udp broadcast");
+		close(sock);
+		return 1;
+	}
+
+	memcpy(outbuf, packet_signature, 2);
+	outbuf[0] = 0xB0;
+	outbuf[1] = 0;
+	outbuf[2] = atoi(argv[1]);
+
+	if (sendto(sock, outbuf, 2 + 3, 0, (struct sockaddr *)&si_other, sizeof(si_other)) == -1) {
+		fprintf(stderr, "Error sending broadcast packet.\n");
+	}
+
+
+        return 0;
+}
--- midi/udpmidi.c
+++ midi/udpmidi.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <arpa/inet.h>
 #include <alsa/asoundlib.h>
 #include <alsa/asoundef.h>
 #include <alsa/global.h>
@@ -14,24 +16,7 @@ snd_rawmidi_t *output;
 
 unsigned char packet_signature[] = { 0x13, 0x37 };
 
-void play() {
-        char buffer[512] = {0};
-        int sz;
-
-        /* Notes: 90 on, 80 off, B0 cc, C0 pc, E0 bend */
-        buffer[0] = 0xB0;
-        buffer[1] = 00;
-        buffer[2] = 00;
-        sz = snd_rawmidi_write(output, buffer, 3);
-	sleep(1);
-        printf("(off)snd:rawmidi:write-%X\n", sz);
-        buffer[0] = 0xB0;
-        buffer[1] = 00;
-        buffer[2] = 0x70;
-        sz = snd_rawmidi_write(output, buffer, 3);
-	sleep(1);
-        printf("(on)snd:rawmidi:write-%X\n", sz);
-}
+#define UDPMIDI_PORT 1205
 
 static void sig_handler(int signal)
 {
@@ -46,14 +31,16 @@ int main(int argc, char **argv) {
 	struct pollfd *pfds;
 	struct sockaddr_in si_me, si_other;
 	int len, slen = sizeof(si_other);
+	int optval = 1;
 
         if(argc < 2) {
-                fprintf(stderr, "%s /dev/input/event<X>\n", argv[0]);
+                fprintf(stderr, "%s <device>\n", argv[0]);
                 exit(-1);
         }
         e = snd_rawmidi_open(&input, &output, argv[1], SND_RAWMIDI_NONBLOCK);//O_WRONLY);
 	if (e == -1) {
-	        printf("snd:rawmidi:open-%X\n", e);
+	        fprintf(stderr, "snd:rawmidi:open-%X\n", e);
+		return 1;
 	}
 
 	if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
@@ -63,8 +50,13 @@ int main(int argc, char **argv) {
 
 	memset((void *) &si_me, 0, sizeof(si_me));
 	si_me.sin_family = AF_INET;
-	si_me.sin_port = htons(1005);
+	si_me.sin_port = htons(UDPMIDI_PORT);
 	si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	memset((void *) &si_other, 0, sizeof(si_me));
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(UDPMIDI_PORT);
+	si_other.sin_addr.s_addr = htonl(INADDR_BROADCAST);
 
 	if (bind(sock, (const struct sockaddr*)&si_me, sizeof(si_me)) == -1) {
 		perror("udp port");
@@ -72,11 +64,17 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	npfds = snd_rawmidi_poll_descriptors_count(input);
+	if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &optval, sizeof (optval)) ==- 1) {
+		perror("udp broadcast");
+		close(sock);
+		return 1;
+	}
+
+	npfds = snd_rawmidi_poll_descriptors_count(input) + 1;
 	pfds = alloca((npfds + 1) * sizeof(struct pollfd));
 	snd_rawmidi_poll_descriptors(input, pfds, npfds);
-	pfds[npfds].fd = sock;
-	pfds[npfds].events = POLLIN;
+	pfds[npfds-1].fd = sock;
+	pfds[npfds-1].events = POLLRDNORM;
 
         while(1) {
 		int i,rlen, midiin;
@@ -84,10 +82,14 @@ int main(int argc, char **argv) {
 
 		midiin = 0;
 		for (i = 0; i < npfds; ++i) {
-			if (pdfs[i].revents & POLLIN)
+			if (pfds[i].revents & POLLIN)
 				midiin = 1;
 		}
 		if (midiin) {
+			unsigned char buf[10];
+			unsigned char outbuf[12];
+
+			printf("got midi\n");
 			rlen = snd_rawmidi_read(input, buf, sizeof(buf));
 			if (rlen == -EAGAIN)
 				continue;
@@ -96,11 +98,19 @@ int main(int argc, char **argv) {
 				break;
 			}
 			// UDP broadcast
+			memcpy(outbuf, packet_signature, 2);
+			if (rlen > 10) continue;
+			memcpy(outbuf + 2, buf, rlen);
+			if (sendto(sock, outbuf, 2 + rlen, 0, (struct sockaddr *)&si_other, sizeof(si_other)) == -1) {
+				fprintf(stderr, "Error sending broadcast packet.\n");
+			}
 		}
 
-		if (pdfs[npfds].revents & POLLIN) {
+		if (pfds[npfds-1].revents & POLLRDNORM) {
 			int len,slen;
-			unsigned char buffer[10]:
+			unsigned char buffer[10];
+
+			printf("Got udp\n");
 			slen = sizeof(si_other);
 			len = recvfrom(sock, buffer, 10, 0, (struct sockaddr *)&si_other, &slen);
 			if (len > 2 && memcmp(buffer, packet_signature, 2) == 0) {
